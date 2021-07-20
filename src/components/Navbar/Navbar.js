9f5483eed2f0730ee86d31825c5daadf588969a3
Anindya Kumar Patro <anindya20012001@gmail.com>
2021-07-20 14:01:44 +0530

    New Repo

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                Navbar
+            </div> 
+        </>
+        
+    )
+}
+
+export default Navbar
