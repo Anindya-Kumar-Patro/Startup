2b315863a84ac64fd6be0f1f5cab07984ad8cb44
Anindya Kumar Patro <anindya20012001@gmail.com>
2021-07-23 21:07:17 +0530

    NavFilter Start

---
+++ src/components/NavFilter/NavFilter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavFilter = () => {
+    return (
+        <>
+           <div className="fullsection">
+            <div className="filtersection">
+
+            </div>
+            <div className="navbarsection">
+
+            </div>
+           </div> 
+        </>
+    )
+}
+
+export default NavFilter
